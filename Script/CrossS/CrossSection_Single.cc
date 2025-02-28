b050b671bd2b7c9b2e667522af9afe3100ac2c32
easel7 <blankeasel7@gmail.com>
2025-02-28 18:14:47 +0100

    update

--- Script/CrossS/CrossSection_Single.cc
+++ Script/CrossS/CrossSection_Single.cc
@@ -1,6 +1,6 @@
 void CrossSection_Single()
 {
-    auto file = TFile::Open("/Users/xiongzheng/software/B4/B4c/build/B4_proton.root");
+    auto file = TFile::Open("/Users/xiongzheng/software/B4/B4c/Root/Proton_100GeV.root");
     auto tree = (TTree*)file->Get("B4");
     auto c1 = new TCanvas("c1","c1",1800,600);
     auto *h0 = new TH1D("h0","h0",40,0,100);
@@ -9,41 +9,40 @@ void CrossSection_Single()
 
     TCut EM  = "First_Type==0";
     TCut HD  = "First_Had_Depth>=0";
-    TCut HI  = "First_Had_Depth>=0 && First_Had_Type==2";
+    TCut HI  = "First_Had_Depth>=0 && First_Had_Type==1";
 
     tree->Draw("First_Depth>>h0",EM,"");
     tree->Draw("First_Had_Depth>>h1",HD,"");
     tree->Draw("First_Had_Depth>>h2",HI,"");
 
-    h0->SetTitle("100 GeV proton - Electromagnetic Depth Distribution;Depth(mm);Counts");
-    h1->SetTitle("100 GeV proton - Hadronic Depth Distribution;Depth(mm);Counts");
-    h2->SetTitle("100 GeV proton - Inelastic Hadronic Depth Distribution;Depth(mm);Counts");
+    h0->SetTitle("100 GeV Proton - Electromagnetic Depth Distribution;Depth(mm);Counts");
+    h1->SetTitle("100 GeV Proton - Hadronic Depth Distribution;Depth(mm);Counts");
+    h2->SetTitle("100 GeV Proton - Inelastic Hadronic Depth Distribution;Depth(mm);Counts");
 
 
-    TF1 *fitFunc0 = new TF1("fitFunc0", "[0]*exp(-x/[1])", 0, 60);
-    TF1 *fitFunc1 = new TF1("fitFunc1", "[0]*exp(-x/[1])", 0, 200);
+    TF1 *fitFunc0 = new TF1("fitFunc0", "[0]*exp(-x/[1])", 0, 70);
+    TF1 *fitFunc1 = new TF1("fitFunc1", "[0]*exp(-x/[1])", 0, 250);
     TF1 *fitFunc2 = new TF1("fitFunc2", "[0]*exp(-x/[1])", 0, 200);
-
-    c1->Clear();
-    c1->Divide(3,1);
-    c1->cd(1);
-    gPad->SetLogy();
-    gStyle->SetOptFit(1111);
-
-    h0->Draw();
-
-
-    fitFunc0->SetParameters(100, 10); // 初始参数：振幅=100, λ=10 cm
-    h0->Fit(fitFunc0, "R"); // 进行拟合
-    double constant0   = fitFunc0->GetParameter(0);
-    double lambda0     = fitFunc0->GetParameter(1);
-    double lambda0_err = fitFunc0->GetParError(1);
 
     TLatex latex;
     latex.SetTextSize(0.04);
     latex.SetTextFont(72);
     latex.SetTextAlign(13);  //align at top
-    latex.DrawLatex(5,3,"Fitting Function: P=[0]*exp(-x/[1])");
+
+    c1->Clear();
+    c1->Divide(3,1);
+
+    c1->cd(1);
+    gPad->SetLogy();
+    gStyle->SetOptFit(1111);
+
+    h0->Draw();
+    fitFunc0->SetParameters(100, 10); // 初始参数：振幅=100, λ=10 cm
+    h0->Fit(fitFunc0, "R"); // 进行拟合
+    double constant0   = fitFunc0->GetParameter(0);
+    double lambda0     = fitFunc0->GetParameter(1);
+    double lambda0_err = fitFunc0->GetParError(1);
+    latex.DrawLatex(5,100,"Fitting Function: P=[0]*exp(-x/[1])");
     
     c1->cd(2);
     gPad->SetLogy();
@@ -55,7 +54,7 @@ void CrossSection_Single()
     double constant1   = fitFunc1->GetParameter(0);
     double lambda1     = fitFunc1->GetParameter(1);
     double lambda1_err = fitFunc1->GetParError(1);
-    latex.DrawLatex(5,100,"Fitting Function: P=[0]*exp(-x/[1])");
+    latex.DrawLatex(5,1000,"Fitting Function: P=[0]*exp(-x/[1])");
 
     c1->cd(3);
     gPad->SetLogy();
@@ -67,9 +66,9 @@ void CrossSection_Single()
     double constant2   = fitFunc2->GetParameter(0);
     double lambda2     = fitFunc2->GetParameter(1);
     double lambda2_err = fitFunc2->GetParError(1);
-    latex.DrawLatex(5,100,"Fitting Function: P=[0]*exp(-x/[1])");
+    latex.DrawLatex(5,1000,"Fitting Function: P=[0]*exp(-x/[1])");
 
-    double n_BGO = TMath::Na()*7.13/1245.8344; // cm-3
+    double n_BGO = TMath::Na()*7.13/ (1245.8344/19.); // cm-3
     double em_section = 1 / lambda0 / n_BGO * 1e25; // barn, mm = 1e-1 cm, 1e24 barn = 1 cm^2
     double em_section_err = em_section * lambda0_err/lambda0; // barn
     double hd_section = 1 / lambda1 / n_BGO * 1e25; // barn, mm = 1e-1 cm, 1e24 barn = 1 cm^2
@@ -77,13 +76,13 @@ void CrossSection_Single()
     double hi_section = 1 / lambda2 / n_BGO * 1e25; // barn, mm = 1e-1 cm, 1e24 barn = 1 cm^2
     double hi_section_err = hi_section * lambda2_err/lambda2; // barn
 
-    cout << "EM Interaction Constant: " << constant0  << ",EM Lambda: " << lambda0 << " mm, EM Lambda_err = " << lambda0_err << " mm " << endl;
-    cout << "Nuclear Collsion Constant: " << constant1  << ",HD Lambda: " << lambda1 << " mm, HD Lambda_err = " << lambda1_err << " mm " << endl;
-    cout << "Nuclear Interaction Constant: " << constant1  << ",HI Lambda: " << lambda2 << " mm, HI Lambda_err = " << lambda2_err << " mm " << endl;
+    cout << "EM Interaction Constant: " << constant0  << ",EM Lambda: " << lambda0 << " ± " << lambda0_err << " mm " << endl;
+    cout << "Nuclear Collsion Constant: " << constant1  << ",HD Lambda: " << lambda1 << " ± " << lambda1_err << " mm " << endl;
+    cout << "Nuclear Interaction Constant: " << constant2  << ",HI Lambda: " << lambda2 << " ± " << lambda2_err << " mm " << endl;
 
     cout << "EM Section : " << em_section << " ± " << em_section_err << " barn" << endl;
     cout << "Nuclear Collsion Section : " << hd_section << " ± " << hd_section_err << " barn" << endl;
     cout << "Nuclear Interaction Section : " << hi_section << " ± " << hi_section_err << " barn" << endl;
 
-    c1->SaveAs("/Users/xiongzheng/software/B4/B4c/Script/CrossS/CrossSection_proton.pdf");
+    c1->SaveAs("/Users/xiongzheng/software/B4/B4c/Script/CrossS/CrossSection_Proton.pdf");
 }
